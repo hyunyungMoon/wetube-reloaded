58ab64c2ff5673f78fdd3d95f2a66d1c4cbfcad9
hyunyungMoon <hymoon.vdrc@gmail.com>
2022-04-12 18:13:22 +0900

    before router

---
+++ src/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import morgan from "morgan";
+
+const PORT = 4000;
+
+const app = express();
+const logger = morgan("dev");
+
+const home = (req, res) => {
+  console.log("i will reponse");
+  return res.send("hello home");
+};
+
+const login = (req, res) => {
+  return res.send("login");
+};
+
+app.use(logger);
+app.get("/", home);
+app.get("/login", login);
+
+const handleListening = () =>
+  console.log(`Server listening on port http://localhost:${PORT} 😊`);
+
+app.listen(PORT, handleListening);
